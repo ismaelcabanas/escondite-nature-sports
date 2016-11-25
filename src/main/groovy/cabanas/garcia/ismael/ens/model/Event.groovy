6f32233837ecc394234ab7a0475141aa6ae1e3d5
XI317311 <ismael.cabanas@extern.isban.com>
2016-11-25 08:29:34 +0100

    Añadido el toString() a la clase

--- src/main/groovy/cabanas/garcia/ismael/ens/model/Event.groovy
+++ src/main/groovy/cabanas/garcia/ismael/ens/model/Event.groovy
@@ -1,11 +1,13 @@
 package cabanas.garcia.ismael.ens.model
 
+import groovy.transform.ToString
 import groovy.transform.builder.Builder
 
 /**
  * This class keep event information
  */
 @Builder
+@ToString
 class Event {
     String name
     String description
