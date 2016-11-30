441add30c77fd5ddac5f2c12f871e5513383ddf9
XI317311 <ismael.cabanas@extern.isban.com>
2016-11-30 09:18:48 +0100

    Refactor

---
+++ src/test/groovy/cabanas/garcia/ismael/ens/util/ErrorPropertiesUtilSpec.groovy
@@ -0,0 +1,21 @@
+package cabanas.garcia.ismael.ens.util
+
+import cabanas.garcia.ismael.ens.ErrorCode
+import spock.lang.Specification
+
+/**
+ * Created by XI317311 on 29/11/2016.
+ */
+class ErrorPropertiesUtilSpec extends Specification {
+
+    def "get message from properties file for error code given"(){
+/*
+        given:
+        String errorCode = ErrorCode.MISSING_REQUIRED_REQUEST_BODY_PARAMETER
+
+        when:
+*/
+
+
+    }
+}
--- src/test/groovy/cabanas/garcia/ismael/ens/util/ErrorPropertiesUtilTest.groovy
+++
@@ -1,7 +0,0 @@
-package cabanas.garcia.ismael.ens.util
-
-/**
- * Created by XI317311 on 29/11/2016.
- */
-class ErrorPropertiesUtilTest {
-}
